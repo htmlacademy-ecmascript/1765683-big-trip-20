e40d31d6a81cc7de1f000f9730cb0a57e4e001b4
r1zyh <kirill.ananiev2012@yandex.ru>
2023-05-09 19:29:30 +0300

    Добавит EmptyListMessage

--- src/presenter/waypoints-presenter.js
+++ src/presenter/waypoints-presenter.js
@@ -4,6 +4,7 @@ import EventEditView from '../view/event-edit-view.js';
 import EventNewView from '../view/event-new-view.js';
 import EventView from '../view/event-view.js';
 import { getRandomArrayElement } from '../util.js';
+import EmptyListMessage from '../view/event-list-empty-view.js';
 
 export default class WaypointPresenter {
   #eventContainer = null;
@@ -21,15 +22,22 @@ export default class WaypointPresenter {
     this.#waypoints = [...this.#waypointsModel.waypoints];
 
     this.#renderEventList();
-    render(
+    /*render(
       new EventEditView({ waypoint: getRandomArrayElement(this.#waypoints) }),
       this.#eventListComponent.element
-    );
-    this.#renderNewEventComponent();
+    );*/
 
-    for (let i = 0; i < this.#waypoints.length; i++) {
-      this.#renderWaypoints(this.#waypoints[i]);
+    if (this.#waypoints.length !== 0) {
+
+      this.#renderNewEventComponent();
+
+      for (let i = 0; i < this.#waypoints.length; i++) {
+        this.#renderWaypoints(this.#waypoints[i]);
+      }
+    } else {
+      this.#renderEmptyListMessage();
     }
+
   }
 
   #renderEventList() {
@@ -43,5 +51,9 @@ export default class WaypointPresenter {
   #renderWaypoints(waypoint) {
     const eventViewComponent = new EventView({ waypoint });
     render(eventViewComponent, this.#eventListComponent.element);
+  }
+
+  #renderEmptyListMessage() {
+    render(new EmptyListMessage, this.#eventListComponent.element);
   }
 }
