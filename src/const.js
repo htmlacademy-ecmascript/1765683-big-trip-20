5772a86be43fbd0e56157369859e2d8b943c794a
r1zyh <kirill.ananiev2012@yandex.ru>
2023-05-20 23:23:29 +0300

    добавит типы сортировки

--- src/const.js
+++ src/const.js
@@ -25,4 +25,12 @@ const OFFERS_PRICE = [20, 50, 140, 200, 600, ];
 
 const TOTAL_PRICE = [20, 160, 600, 830];
 
-export { TRAVEL_WAYPOINTS, WAYPOINT_OPTIONS, TOTAL_PRICE, OFFERS, OFFERS_PRICE };
+const SORT_TYPES = {
+  day: 'day',
+  event: 'event',
+  time: 'time',
+  price: 'price',
+  offers: 'offers',
+};
+
+export { TRAVEL_WAYPOINTS, WAYPOINT_OPTIONS, TOTAL_PRICE, OFFERS, OFFERS_PRICE, SORT_TYPES };
