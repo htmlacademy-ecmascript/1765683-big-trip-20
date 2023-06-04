85284ecb1138ad2d7f0092e542d0d56010cfd324
r1zyh <kirill.ananiev2012@yandex.ru>
2023-06-04 18:13:10 +0300

    Добавит модель для фильтров

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import WaypointsPresenter from './presenter/waypoints-presenter.js';
 import WaypointsModel from './model/waypoints-model.js';
 import { generateFilter } from './mock/filter.js';
 import FiltersPresenter from './presenter/filters-presenter.js';
+import FilterModel from './model/filter-model.js';
 
 const waypointsContainer = document.querySelector('main .trip-events');
 const filtersContainer = document.querySelector('header .trip-controls__filters');
 
 
 const waypointsModel = new WaypointsModel();
+const filterModel = new FilterModel();
 
 const filters = generateFilter(waypointsModel.waypoints);
 const filtersPresenter = new FiltersPresenter({ filtersContainer });
